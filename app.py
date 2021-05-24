bda319d4fbbc5cfe5cdc8860f92354369e789849
keimos <keithmosleyjr@gmail.com>
2021-05-23 21:56:38 -0400

    Added file to print 3 cities

---
+++ app.py
@@ -0,0 +1,9 @@
+# This is a Python script for Favorite City
+
+def favorite_city(name):
+    print("One of my favorite cities is " + name)
+
+
+favorite_city("Chicago, Illinois")
+favorite_city("Miami, Florida")
+favorite_city("Denver, Colorado")
