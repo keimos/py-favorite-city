bbb6fb2a9b0cd78529d62d5e7de6c80ce6c69392
keimos <keithmosleyjr@gmail.com>
2025-03-17 14:51:02 -0400

    Deleted app.py file

--- app.py
+++
@@ -1,9 +0,0 @@
-# This is a Python script for Favorite City
-
-def favorite_city(name):
-    print("One of my favorite cities is " + name)
-
-
-favorite_city("Chicago, Illinois")
-favorite_city("Miami, Florida")
-favorite_city("Denver, Colorado")
